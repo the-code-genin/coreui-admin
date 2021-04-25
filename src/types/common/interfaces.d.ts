0901bd523196c6dea2ac0802a7a50c8cd4d38ab0
Mohammed Adekunle <adekunle3317@gmail.com>
2021-04-25 04:08:50 +0100

    Implemented type roots

---
+++ src/types/common/interfaces.d.ts
@@ -0,0 +1,19 @@
+// Declare interfaces
+
+declare module 'nanobar' {
+    export default class Nanobar {
+        go(to: number): void
+    }
+}
+
+declare interface CoreUIStoreState {
+    sidebarShow: 'responsive' | boolean,
+    sidebarMinimize: boolean,
+    [key: string]: any
+}
+
+// Static modules
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.svg';
+declare module '*.json';

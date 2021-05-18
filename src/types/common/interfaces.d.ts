7cf01e41118d51742373da774a5102cadff95b8d
Mohammed Adekunle <adekunle3317@gmail.com>
2021-05-18 13:50:05 +0100

    Updated types

--- src/types/common/interfaces.d.ts
+++ src/types/common/interfaces.d.ts
@@ -12,6 +12,18 @@ declare interface CoreUIStoreState {
     [key: string]: any
 }
 
+declare interface ApiResponse {
+    success: boolean,
+    payload: {
+        [key: string]: any
+    },
+    error: {
+        code: number,
+        type: string,
+        message: string,
+    }
+}
+
 // Static modules
 declare module '*.png';
 declare module '*.jpg';

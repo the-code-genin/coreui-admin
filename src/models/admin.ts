f5d12fe86648b7945393b39e20bae57a7a0cc71d
Mohammed Adekunle <adekunle3317@gmail.com>
2021-05-18 13:49:15 +0100

    Implemented admin model

---
+++ src/models/admin.ts
@@ -0,0 +1,29 @@
+import moment, { Moment } from 'moment'
+
+export default class Admin {
+    readonly created_at: Moment
+    readonly updated_at: Moment
+
+    constructor(
+        readonly id: number,
+        readonly name: string,
+        readonly email: string,
+        readonly status: 'active'|'banned',
+        created_at: string,
+        updated_at: string
+    ) {
+        this.created_at = moment.utc(created_at);
+        this.updated_at = moment.utc(updated_at);
+    }
+
+    static fromJson(json: any): Admin {
+        return new Admin(
+            json.id,
+            json.name,
+            json.email,
+            json.status,
+            json.created_at,
+            json.updated_at
+        );
+    }
+}

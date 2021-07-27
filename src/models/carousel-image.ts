7966b9c88ff3d5ff8e72f061660e5e6cec076590
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-27 23:42:57 +0100

    implemented carousel image model

---
+++ src/models/carousel-image.ts
@@ -0,0 +1,27 @@
+import moment, { Moment } from 'moment'
+
+export default class CarouselImage {
+    readonly created_at: Moment
+    readonly updated_at: Moment
+
+    constructor(
+        readonly id: number,
+        readonly title: string,
+        readonly image: string,
+        created_at: string,
+        updated_at: string
+    ) {
+        this.created_at = moment.utc(created_at);
+        this.updated_at = moment.utc(updated_at);
+    }
+
+    static fromJson(json: any): CarouselImage {
+        return new CarouselImage(
+            json.id,
+            json.title,
+            json.image,
+            json.created_at,
+            json.updated_at
+        );
+    }
+}

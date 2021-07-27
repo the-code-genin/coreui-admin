48d13bf5f153974f6f99fa6d389e7631ad106cf7
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-27 23:56:30 +0100

    Refactored auth routes

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -13,24 +13,24 @@ export default [
                 },
                 component: () => import('@/views/auth/Login.vue'),
             },
-            // {
-            //     path: '/forgot-password',
-            //     name: 'ForgotPassword',
-            //     meta: {
-            //         guest: true,
-            //     },
-            //     component: () => import('@/views/auth/ForgotPassword.vue'),
-            // },
-            // {
-            //     path: '/reset-password',
-            //     name: 'ResetPassword',
-            //     component: () => import('@/views/auth/ResetPassword.vue'),
-            // },
-            // {
-            //     path: '/password-reset',
-            //     name: 'PasswordReset',
-            //     component: () => import('@/views/auth/PasswordReset.vue'),
-            // },
+            {
+                path: '/forgot-password',
+                name: 'ForgotPassword',
+                meta: {
+                    guest: true,
+                },
+                component: () => import('@/views/auth/ForgotPassword.vue'),
+            },
+            {
+                path: '/reset-password',
+                name: 'ResetPassword',
+                component: () => import('@/views/auth/ResetPassword.vue'),
+            },
+            {
+                path: '/password-reset',
+                name: 'PasswordReset',
+                component: () => import('@/views/auth/PasswordReset.vue'),
+            },
         ]
     }
 ];

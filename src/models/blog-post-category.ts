9f88a3899aac81162228a658024bb1aff5f23e89
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-27 23:44:43 +0100

    Implemented blog post category model

---
+++ src/models/blog-post-category.ts
@@ -0,0 +1,29 @@
+import moment, { Moment } from 'moment'
+
+export default class BlogPostCategory {
+    readonly created_at: Moment
+    readonly updated_at: Moment
+
+    constructor(
+        readonly id: number,
+        readonly name: string,
+        readonly no_blog_posts: number,
+        readonly no_published_blog_posts: number,
+        created_at: string,
+        updated_at: string
+    ) {
+        this.created_at = moment.utc(created_at);
+        this.updated_at = moment.utc(updated_at);
+    }
+
+    static fromJson(json: any): BlogPostCategory {
+        return new BlogPostCategory(
+            json.id,
+            json.name,
+            json.no_blog_posts,
+            json.no_published_blog_posts,
+            json.created_at,
+            json.updated_at
+        );
+    }
+}
